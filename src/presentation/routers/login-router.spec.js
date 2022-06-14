23fca1f3a200c444a4651dbc49f58f84ebfa487a
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-06-14 17:05:07 -0300

    :tada: my first test

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,16 @@
+class LoginRouter {
+  route () {}
+}
+
+describe('Login Roter', () => {
+  test('Should return 400 if email is not provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
