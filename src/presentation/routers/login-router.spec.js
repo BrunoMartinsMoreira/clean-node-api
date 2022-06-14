a4a702cb45ea4915293ce2751ee20dabf5cc2c08
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-06-14 17:29:02 -0300

    add teste para password no login router

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -13,7 +13,18 @@ describe('Login Roter', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
-        password: 'any'
+        password: 'anypwd'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if password is not provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any@any.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
