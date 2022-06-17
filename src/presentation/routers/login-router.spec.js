6cf80a28c7153ff158b10d53947aa8472b8a6d8b
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-06-16 21:56:16 -0300

    valida que LoginRouter retorne 200 quando as credenciais corretas são informadas

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -3,13 +3,15 @@ const MissingParamError = require('../helpers/missing-param-error')
 const UnauthorizedError = require('../helpers/unauthorized-error')
 
 const makeSut = () => {
-  class AuthUseCase {
+  class AuthUseCaseSpy {
     auth (email, password) {
       this.email = email
       this.password = password
+      return this.accessToken
     }
   }
-  const authUseCaseSpy = new AuthUseCase()
+  const authUseCaseSpy = new AuthUseCaseSpy()
+  authUseCaseSpy.accessToken = 'valid_token'
   const sut = new LoginRouter(authUseCaseSpy)
   return {
     authUseCaseSpy,
@@ -68,8 +70,9 @@ describe('Login Roter', () => {
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
 
-  test('Should return 401 when invalid credentials are invalid', () => {
-    const { sut } = makeSut()
+  test('Should return 401 when invalid credentials are provided', () => {
+    const { sut, authUseCaseSpy } = makeSut()
+    authUseCaseSpy.accessToken = null
     const httpRequest = {
       body: {
         email: 'invalid_email@mail.com',
@@ -79,6 +82,18 @@ describe('Login Roter', () => {
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
+  })
+
+  test('Should return 200 when valid credentials are provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'valid_email@mail.com',
+        password: 'valid_pwd'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
   })
 
   test('Should return 500 if AuthUseCase is not provided', () => {
